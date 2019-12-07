38f01e7235cee7bed5ee0417abf9731bff2760a0
MegaJoshy <joshp2865@gmail.com>
2019-12-07 15:55:45 +0000

    yes

---
+++ commands/textme.js
@@ -0,0 +1,16 @@
+exports.run = (client, message, args) => {	
+//		const apiKey = 'BLANK';
+//		const telnyx = require('telnyx')(apiKey);
+
+/*		telnyx.messages.create({
+ 			 'from': 'Livida',
+ 			 'to': '+447470967099',
+ 			 'text': `${message.author.tag} has just sent you a message!`,
+ 			 'messaging_profile_id': 'a7ad45a1-aad7-4084-8ce9-f3391fbd8d53'
+		}).then(function(response){
+		  const message = response.data; // asynchronously handled
+	});
+		
+*/
+	message.channel.send('Ehmm no');
+};
