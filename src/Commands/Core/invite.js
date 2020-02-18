2191d69c0dc635d9be5b7686e0e83c4c5eed9412
MegaJoshy <joshp2865@gmail.com>
2020-02-18 22:51:38 +0000

    Invite bot command using client id

---
+++ src/Commands/Core/invite.js
@@ -0,0 +1,22 @@
+const { RichEmbed } = require("discord.js");
+
+module.exports = {
+    name: "invite",
+    aliases: [],
+    category: "Core",
+    description: "Shows the invite for the current bot",
+    usage: "",
+    permissions: "SEND_MESSAGES",
+    clientPerms: "SEND_MESSAGES",
+    creatorOnly: false,
+    guildOnly: false,
+    premiumOnly: false,
+    requiresArgs: false,
+    run: async (client, msg, args) => {
+        const embed = new RichEmbed()
+        .setTitle("Invite")
+        .setColor(msg.guild.me.highestRole.color || "BLUE")
+        .setDescription(`If you would like to invite the bot you can do that [here](https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`)
+        msg.channel.send(embed)
+    }
+};
