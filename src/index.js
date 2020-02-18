1aeb2908feb8bdf0cb7bc6c6b61a0d7faf4cc5c8
MegaJoshy <joshp2865@gmail.com>
2020-02-18 23:03:44 +0000

    Added Sentry error tracking

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const client = new Bot({
 	disableEveryone: false,
 	sync: true,
 });
+const Sentry = require('@sentry/node');
+Sentry.init({ dsn: 'https://25416379e40f4312ad4a5b56e8a72edb@sentry.io/2648219' });
 
 client.connectToDB();
 client.loadEvents();
