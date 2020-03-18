5ef51240caa30c179e27fbd60f8a84a88c3ca3c3
Callum <callumokane123@gmail.com>
2020-03-18 13:00:01 +0000

    Logs for all guilds

--- src/Events/Guild/message.js
+++ src/Events/Guild/message.js
@@ -4,6 +4,13 @@ const Sentry = require('@sentry/node')
 module.exports = {
   name: 'message',
   run: async (client, msg) => {
+    var { Logs } = require(`${process.cwd()}/src/Structures/Constants/Models.js`),
+      query = {guildID: guild.id};
+    Logs.findOneAndUpdate(query, {guildID: guild.id}, {upsert: true}, function(err, doc) {
+      if (err) return msg.channel.send(err);
+    });
+
+
     if (msg.author.bot || !msg.guild) return
 
     const confPrefix = await client.Models.Prefix.findOne({
