69b2e742acc209735a7e95da2f39e3d95eb0e927
Joshua Pemberton <joshp2865@gmail.com>
2020-03-18 17:18:25 +0000

    adding prefix to presence

--- src/Events/Client/ready.js
+++ src/Events/Client/ready.js
@@ -3,6 +3,9 @@ const { ErelaClient, Utils } = require('erela.js');
 module.exports = {
   name: 'ready',
   run: async (client) => {
+    const Prefix = await client.Models.Prefix.findOne({
+      guildID: msg.guild.id
+    })
     if (client.musicEnabled === true) {
       client.music = new ErelaClient(client, client.nodes)
         .on('nodeError', console.error)
@@ -19,7 +22,7 @@ module.exports = {
         .set('high', 0.25)
     };
 
-    client.user.setPresence({ game: { name: `-help | ${client.guilds.size} guilds` }, status: 'online' })
+    client.user.setPresence({ game: { name: `${prefix}help | ${client.guilds.size} guilds` }, status: 'online' })
     client.log(`${client.user.tag} is online with ${client.guilds.size} guilds logged!`)
 
     // Start express server
