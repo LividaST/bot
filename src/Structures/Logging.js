7ea3659ffd168b736f00ec54fff2254ed079286d
Callum <callumokane123@gmail.com>
2020-03-18 01:59:56 +0000

    Forgot to save :/

--- src/Structures/Logging.js
+++ src/Structures/Logging.js
@@ -14,7 +14,7 @@ client
         if(!config.channelUpdatesEnabled) return;
         let embed = new client.Embed()
             .setTitle("Logs • Channel Creation")
-            .setDescription(`<#${channel.id}> has been created by **${executer("CHANNEL_CREATE", channel.guild.id)}`)
+            .setDescription(`<#${channel.id}> has been created by **${await executer("CHANNEL_CREATE", channel.guild.id)}`)
             .setTimestamp();
         if(config.channelUpdatesChannel.toLowerCase() === "not set") return;
         client.channels.get(config.channelUpdatesChannel).send(embed);
@@ -27,7 +27,7 @@ client
         if(!config.channelUpdatesEnabled) return;
         let embed = new client.Embed()
             .setTitle("Logs • Channel Deleted")
-            .setDescription(`<#${channel.id}> has been deleted by **${executer("CHANNEL_CREATE", channel.guild.id)}`)
+            .setDescription(`<#${channel.id}> has been deleted by **${await executer("CHANNEL_CREATE", channel.guild.id)}`)
             .setTimestamp();
         if(config.channelUpdatesChannel.toLowerCase() === "not set") return;
         client.channels.get(config.channelUpdatesChannel).send(embed);
@@ -39,7 +39,7 @@ client
         if(!config.emojiUpdatesEnabled) return;
         let embed = new client.Embed()
             .setTitle("Logs • Emoji Created")
-            .setDescription(`An emoji called \`${emoji.name}\` has been created by **${executer("EMOJI_CREATE", channel.guild.id)}`)
+            .setDescription(`An emoji called \`${emoji.name}\` has been created by **${await executer("EMOJI_CREATE", channel.guild.id)}`)
             .setTimestamp();
         if(config.emojiUpdatesChannel.toLowerCase() === "not set") return;
         client.channels.get(config.emojiUpdatesChannel).send(embed);  
@@ -51,7 +51,7 @@ client
         if(!config.emojiUpdatesEnabled) return;
         let embed = new client.Embed()
             .setTitle("Logs • Emoji Deleted")
-            .setDescription(`An emoji called \`${emoji.name}\` has been deleted by **${executer("EMOJI_DELETE", channel.guild.id)}`)
+            .setDescription(`An emoji called \`${emoji.name}\` has been deleted by **${await executer("EMOJI_DELETE", channel.guild.id)}`)
             .setTimestamp();
         if(config.emojiUpdatesChannel.toLowerCase() === "not set") return;
         client.channels.get(config.emojiUpdatesChannel).send(embed);  
@@ -63,7 +63,7 @@ client
         if(!config.emojiUpdatesEnabled) return;
         let embed = new client.Embed()
             .setTitle("Logs • Emoji Updated")
-            .setDescription(`An emoji called \`${oldEmoji.name}\` has been updated by **${executer("EMOJI_CREATE", channel.guild.id)}, the emoji's new name is \`${newEmoji.name}\``)
+            .setDescription(`An emoji called \`${oldEmoji.name}\` has been updated by **${await await executer("EMOJI_CREATE", channel.guild.id)}, the emoji's new name is \`${newEmoji.name}\``)
             .setTimestamp();
         if(config.emojiUpdatesChannel.toLowerCase() === "not set") return;
         client.channels.get(config.emojiUpdatesChannel).send(embed);  
