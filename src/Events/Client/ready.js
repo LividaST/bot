35a3a14eb3cd39645ffd09e549efae4c39c502e1
Joshua Pemberton <joshp2865@gmail.com>
2020-03-18 17:23:23 +0000

    testing

--- src/Events/Client/ready.js
+++ src/Events/Client/ready.js
@@ -3,9 +3,7 @@ const { ErelaClient, Utils } = require('erela.js');
 module.exports = {
   name: 'ready',
   run: async (client) => {
-    const Prefix = await client.Models.Prefix.findOne({
-      guildID: msg.guild.id
-    })
+    const Prefix = await client.Models.Prefix.findOne()
     if (client.musicEnabled === true) {
       client.music = new ErelaClient(client, client.nodes)
         .on('nodeError', console.error)
@@ -22,7 +20,7 @@ module.exports = {
         .set('high', 0.25)
     };
 
-    client.user.setPresence({ game: { name: `${prefix}help | ${client.guilds.size} guilds` }, status: 'online' })
+    client.user.setPresence({ game: { name: `${Prefix}help | ${client.guilds.size} guilds` }, status: 'online' })
     client.log(`${client.user.tag} is online with ${client.guilds.size} guilds logged!`)
 
     // Start express server
