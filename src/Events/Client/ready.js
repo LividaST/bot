45b38ef4c9d74a26d30581dcffc87d273e34f74a
Callum <callumokane123@gmail.com>
2020-04-17 00:13:55 +0100

    get me some coffee

--- src/Events/Client/ready.js
+++ src/Events/Client/ready.js
@@ -19,7 +19,7 @@ module.exports = {
         .set('high', 0.25)
     };
 
-    client.user.setPresence({ game: { name: `${process.env.PREFIX}help | ${client.guilds.size} guilds` }, status: 'online' })
+    client.user.setActivity({ game: { name: `${process.env.PREFIX}help | ${client.guilds.size} guilds` }, status: 'online' })
     client.log(`${client.user.tag} is online with ${client.guilds.size} guilds logged!`)
 
     // Start express server
