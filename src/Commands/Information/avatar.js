9ea5a0be52bc00c6d779981b9238afb0d080b44c
Callum <callumokane123@gmail.com>
2020-04-17 02:07:18 +0100

    begone thot

--- src/Commands/Information/avatar.js
+++ src/Commands/Information/avatar.js
@@ -26,7 +26,6 @@ module.exports = {
       const embed = new client.Embed()
         .setAuthor(`${user.tag}'s avatar • Requested by ${msg.author.tag}`, msg.author.avatarURL())
         .setDescription(`\`\`\`${user.avatarURL()}\`\`\``)
-        .setColor(msg.guild.me.roles.color || 'PURPLE')
         .setImage(user.avatarURL())
       msg.channel.send(embed)
     } catch {
