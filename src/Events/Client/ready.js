7b9d09329c764f1fd6b7faf5609ec695a2f09e10
Callum <callumokane123@gmail.com>
2020-04-17 01:21:30 +0100

    ready fixed

--- src/Events/Client/ready.js
+++ src/Events/Client/ready.js
@@ -20,7 +20,7 @@ module.exports = {
     };
 
     client.user.setActivity(`${client.guilds.cache.size} guilds • ${process.env.PREFIX}help`, {type: "WATCHING"})
-    client.log(`${client.user.tag} is online with ${client.guilds.size} guilds logged!`)
+    client.log(`${client.user.tag} is online with ${client.guilds.cache.size} guilds logged!`)
     require(`${process.cwd()}/src/server.js`);
     require(`${process.cwd()}/src/Structures/Logging.js`);
   }
