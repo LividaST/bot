75dcb165046c23ba2c34060d0f11bae189e56dad
Callum <callumokane123@gmail.com>
2020-04-19 05:22:44 +0100

    lala

--- src/Commands/Radio/bind.js
+++ src/Commands/Radio/bind.js
@@ -13,7 +13,7 @@ module.exports = {
     run: async (client, msg, args) => {
         const { RadioBindings } = require(`${process.cwd()}/src/Structures/Constants/Models.js`);
         const data = await RadioBindings.find({guildID: msg.guild.id});
-        if(data[0].channelID === client.getChannel(msg, args[0]).id) return msg.channel.send({embed: {description: "The bot is already bound to this channel!"}});
+        if(data[0].channelID === client.getChannel(msg, args[0]).id && data[0].binded) return msg.channel.send({embed: {description: "The bot is already bound to this channel!"}});
         if(client.getChannel(msg, args[0]).type !== "voice") return msg.reply("the specified channel is not a voice channel!");
         var query = {channelID: client.getChannel(msg, args[0]).id, binded: true};
         RadioBindings.update({guildID: msg.guild.id}, query, {upsert: true}, function(err, doc) {
