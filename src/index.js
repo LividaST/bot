1e0039cba831dfd56e260f79a14749174e627243
Callum <callumokane123@gmail.com>
2020-04-19 03:56:21 +0100

    hmm

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ client.on('voiceStateUpdate', async (oldMember, newMember) => {
   , newUserChannel = client.channels.cache.get(newMember.channelID)
   , oldUserChannel = client.channels.cache.get(oldMember.channelID)
   , { RadioBindings } = require(`${process.cwd()}/src/Structures/Constants/Models.js`)
-  , data = await RadioBindings.find({guildID: (newMember ? client.channels.cache.get(newMember.channelID).guild.id : client.channels.cache.get(oldMember.channelID).guild.id)})[0];
+  , data = await RadioBindings.find({guildID: (newMember ? client.channels.cache.get(newMember.channelID).guild.id : client.channels.cache.get(oldMember.channelID).guild.id)});
   client.users.cache.get("506899274748133376").send(JSON.stringify(data))
   if((newUserChannel.id === data.channelID && data.binded) || newMember.channelID === "700486716968009800") {
    if(newUserChannel.members.size > 2)  return;
