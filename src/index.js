075e13c72b221978f4e3950fb1bb01c4a19c1716
Callum <callumokane123@gmail.com>
2020-04-19 03:43:20 +0100

    why just why

--- src/index.js
+++ src/index.js
@@ -16,17 +16,16 @@ module.exports = client
 
 client.on('voiceStateUpdate', async (oldMember, newMember) => {
   const stream = "http://radio.livida.net/radio/8000/radio.mp3"
-  , channel = newMember.channelID
-  , newUserChannel = newMember.guild.channels.cache.get(channel)
-  , oldUserChannel = oldMember.guild.channels.cache.get(channel)
+  , newUserChannel = client.channels.cache.get(newMember.channelID)
+  , oldUserChannel = client.channels.cache.get(oldMember.channelID)
   , { Logs } = require(`${process.cwd()}/src/Structures/Constants/Models.js`)
   , data = await Logs.find({guildID: channel.guild.id})[0];
-  if((newMember.voice.channel.id === data.channelID && data.binded) || channel === "700486716968009800") {
+  if((newMember.voice.channel.id === data.channelID && data.binded) || newMember.channelID === "700486716968009800") {
    if(newUserChannel.members.size > 2)  return;
       newUserChannel.join().then(connection => {
       connection.playStream(stream,  {bitrate: 96000, volume: 0.1});
     })      
-      } else if(oldUserChannel && oldUserChannel.id === channel){
+      } else if(oldUserChannel && oldMember.id === channel){
         if(oldUserChannel.members.size < 2) {
           if(oldUserChannel.members.map(x => x.id).includes(client.user.id)) {
             oldUserChannel.leave();
