db57e444cc78e6ac8d3546fdc45d37ef7b88c4d6
Joshua Pemberton <joshp2865@gmail.com>
2020-04-27 20:42:53 +0100

    will you work now?

--- src/Events/Guild/message.js
+++ src/Events/Guild/message.js
@@ -64,10 +64,12 @@ module.exports = {
         client.log(err);
         client.Errors.unknownErr(msg, err);
         Sentry.configureScope(function (scope) {
-          scope.setUser({
-            "id": msg.author.id,
-            "username": msg.author.tag
-          });
+          scope.setTag("userid", msg.author.id);
+          scope.setTag("username", msg.author.tag);
+          if(msg.guild.available) {
+            scope.setTag("serverid", msg.guild.id);
+            scope.setTag("server", msg.guild.name);
+          }
         });
         Sentry.captureException(err);
       };
