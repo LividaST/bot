63eafb815c23b8f15476ea30fd45382e9e47d145
Joshua Pemberton <joshp2865@gmail.com>
2020-04-27 19:57:00 +0100

    error spitage

---
+++ src/Commands/Core/error.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'error',
+    aliases: [],
+    category: 'Core',
+    description: 'I spit out an error.',
+    usage: '',
+    permissions: 'SEND_MESSAGES',
+    clientPerms: 'SEND_MESSAGES',
+    creatorOnly: true,
+    guildOnly: false,
+    premiumOnly: false,
+    requiresArgs: false,
+    run: async (client, msg, args) => {
+        myUndefinedFunction();
+        msg.channel.send('I\'ve spat out an error')
+    }
+  }
+  
