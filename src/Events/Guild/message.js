e2b8d81c20aa70ed70e37108a32314b7662f8646
Joshua Pemberton <joshp2865@gmail.com>
2020-04-27 20:16:44 +0100

    what makes you beatiful

--- src/Events/Guild/message.js
+++ src/Events/Guild/message.js
@@ -3,32 +3,49 @@ const Sentry = require('@sentry/node');
 module.exports = {
   name: 'message',
   run: async (client, msg) => {
-  if(msg.guild) {
-    var { Logs, RadioBindings } = require(`${process.cwd()}/src/Structures/Constants/Models.js`),
-    query1 = {guildID: msg.guild.id},
-    query2 = {guildID: msg.guild.id};
-    Logs.findOneAndUpdate(query1, {guildID: msg.guild.id}, {upsert: true}, function(err, doc) {
-      if (msg.author.bot) return
-      if (err) return msg.channel.send("err " + err);
-    });
-    RadioBindings.findOneAndUpdate(query2, {guildID: msg.guild.id}, {upsert: true}, function(err, doc) {
-      if (msg.author.bot) return
-      if (err) return msg.channel.send("err " + err);
-    });
-  }
-  if (msg.author.bot) return
-  const confPrefix = await client.Models.Prefix.findOne({
-      guildID: msg.guild.id
-    })
-    , prefixMention = new RegExp(`^<@!?${client.user.id}> `)
-    , prefix = msg.content.match(prefixMention) ? msg.content.match(prefixMention)[0] : confPrefix ? confPrefix.prefix : client.prefix;
+    if (msg.guild) {
+      var {
+        Logs,
+        RadioBindings
+      } = require(`${process.cwd()}/src/Structures/Constants/Models.js`),
+        query1 = {
+          guildID: msg.guild.id
+        },
+        query2 = {
+          guildID: msg.guild.id
+        };
+      Logs.findOneAndUpdate(query1, {
+        guildID: msg.guild.id
+      }, {
+        upsert: true
+      }, function (err, doc) {
+        if (msg.author.bot) return
+        if (err) return msg.channel.send("err " + err);
+      });
+      RadioBindings.findOneAndUpdate(query2, {
+        guildID: msg.guild.id
+      }, {
+        upsert: true
+      }, function (err, doc) {
+        if (msg.author.bot) return
+        if (err) return msg.channel.send("err " + err);
+      });
+    }
+    if (msg.author.bot) return
+    const confPrefix = await client.Models.Prefix.findOne({
+        guildID: msg.guild.id
+      }),
+      prefixMention = new RegExp(`^<@!?${client.user.id}> `),
+      prefix = msg.content.match(prefixMention) ? msg.content.match(prefixMention)[0] : confPrefix ? confPrefix.prefix : client.prefix;
     if (msg.content.startsWith(prefix)) {
-      const args = msg.content.slice(prefix.length).trim().split(' ')
-      , cmd = args.shift().toLowerCase()
+      const args = msg.content.slice(prefix.length).trim().split(' '),
+        cmd = args.shift().toLowerCase()
       try {
         const command = client.commands.has(cmd) ? client.commands.get(cmd) : client.commands.get(client.aliases.get(cmd))
         if (command) {
-          if (command.premiumOnly === true && await client.Models.Premium.findOne({ guildID: msg.guild.id }) === null) return client.Errors.premiumOnly(msg.channel);
+          if (command.premiumOnly === true && await client.Models.Premium.findOne({
+              guildID: msg.guild.id
+            }) === null) return client.Errors.premiumOnly(msg.channel);
           if (command.permissions && !msg.member.hasPermission(command.permissions) && !client.creators.ids.includes(msg.author.id)) return client.Errors.noPerms(msg.channel, command.permissions);
           if (command.clientPerms && !msg.guild.me.hasPermission(command.clientPerms)) return client.Errors.noClientPerms(msg.channel, command.clientPerms);
           if (command.requiresArgs === true && args.length < 1) return client.Errors.noArgs(msg.guild, msg.channel, command.name);
@@ -46,13 +63,13 @@ module.exports = {
       } catch (err) {
         client.log(err);
         client.Errors.unknownErr(msg, err);
-        Sentry.configureScope(function(scope) {
+        Sentry.configureScope(function (scope) {
           scope.setUser({
             "id": msg.author.id,
             "username": msg.author.tag
           });
-          Sentry.captureException(err);  
-        });                
+        });
+        Sentry.captureException(err);
       };
     } else {
       if (msg.content.match(new RegExp(`^<@!?${client.user.id}>( |)$`))) {
