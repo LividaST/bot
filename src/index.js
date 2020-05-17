8fe3e1295256d152b060caa6bf93f3872134ef55
Joshua Pemberton <joshp2865@gmail.com>
2020-05-17 11:19:17 +0100

    commented radio bind for now

--- src/index.js
+++ src/index.js
@@ -11,32 +11,29 @@ Sentry.init({
 });
 client.start();
 module.exports = client
-const { ErelaClient } = require("erela.js");
-const nodes = require("../nodes.json");
-client.music = new ErelaClient(client, nodes);
 
-client.on('voiceStateUpdate', async (oldMember, newMember) => {
-  const stream = "https://radio.risefm.net/radio/8000/radio.mp3"
-  , newUserChannel = client.channels.cache.get(newMember.channelID)
-  , oldUserChannel = client.channels.cache.get(oldMember.channelID)
-  , { RadioBindings } = require(`${process.cwd()}/src/Structures/Constants/Models.js`)
-  , data = await RadioBindings.find({guildID: (newMember.channelID ? client.channels.cache.get(newMember.channelID).guild.id : client.channels.cache.get(oldMember.channelID).guild.id)});
-  if((newMember.channelID == data[0].channelID && data[0].binded) || newMember.channelID == "700486716968009800") {
-   if(newUserChannel.members.size > 2)  return;
-      const player = client.music.players.spawn({
-        guild: msg.guild,
-        voiceChannel: newUserChannel,
-        textChannel: msg.channel
-       }); 
-       {
-      player.queue.add('https://radio.risefm.net/radio/8000/radio.mp3')
-      if (!player.playing) player.play();
-    }      
-      } else if(oldUserChannel && oldMember.channelID == data[0].channelID){
-        if(oldUserChannel.members.size < 2) {
-          if(oldUserChannel.members.map(x => x.id).includes(client.user.id)) {
-            oldUserChannel.leave();
-          }
-        }
-      }
-})
+// client.on('voiceStateUpdate', async (oldMember, newMember) => {
+//   const stream = "https://radio.risefm.net/radio/8000/radio.mp3"
+//   , newUserChannel = client.channels.cache.get(newMember.channelID)
+//   , oldUserChannel = client.channels.cache.get(oldMember.channelID)
+//   , { RadioBindings } = require(`${process.cwd()}/src/Structures/Constants/Models.js`)
+//   , data = await RadioBindings.find({guildID: (newMember.channelID ? client.channels.cache.get(newMember.channelID).guild.id : client.channels.cache.get(oldMember.channelID).guild.id)});
+//   if((newMember.channelID == data[0].channelID && data[0].binded) || newMember.channelID == "700486716968009800") {
+//    if(newUserChannel.members.size > 2)  return;
+//       const player = client.music.players.spawn({
+//         guild: msg.guild,
+//         voiceChannel: newUserChannel,
+//         textChannel: msg.channel
+//        }); 
+//        {
+//       player.queue.add('https://radio.risefm.net/radio/8000/radio.mp3')
+//       if (!player.playing) player.play();
+//     }      
+//       } else if(oldUserChannel && oldMember.channelID == data[0].channelID){
+//         if(oldUserChannel.members.size < 2) {
+//           if(oldUserChannel.members.map(x => x.id).includes(client.user.id)) {
+//             oldUserChannel.leave();
+//           }
+//         }
+//       }
+// })
