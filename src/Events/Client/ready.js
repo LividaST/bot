3cc0ed8c26fab975b5cc30a13bc22cf2d214fa47
Joshua Pemberton <joshp2865@gmail.com>
2020-05-17 09:14:32 +0100

    added ready events for erela.js

--- src/Events/Client/ready.js
+++ src/Events/Client/ready.js
@@ -5,5 +5,12 @@ module.exports = {
     client.log(`${client.user.tag} is online with ${client.guilds.cache.size} guilds logged!`)
     require(`${process.cwd()}/src/server.js`);
     require(`${process.cwd()}/src/Structures/Logging.js`);
+    client.music.on("nodeConnect", node => console.log("New node connected"));
+    client.music.on("nodeError", (node, error) => console.log(`Node error: ${error.message}`));
+    client.music.on("trackStart", (player, track) => player.textChannel.send(`Now playing: ${track.title}`));
+    client.music.on("queueEnd", player => {
+        player.textChannel.send("Queue has ended.")
+        client.music.players.destroy(player.guild.id);
+    });
   }
 }
