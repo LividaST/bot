b0ba14e46b10ab83ad8ac0fd5dd22cf130fd7137
Joshua Pemberton <joshp2865@gmail.com>
2020-05-17 11:38:30 +0100

    pause command

---
+++ src/Commands/Music/pause.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'pause',
+    aliases: ['unpause', 'resume'],
+    category: 'Music',
+    description: 'Pause or unpause the music!',
+    usage: '',
+    permissions: 'SEND_MESSAGES',
+    clientPerms: 'SEND_MESSAGES',
+    creatorOnly: false,
+    guildOnly: true,
+    premiumOnly: false,
+    run: async (client, msg, args) => {
+      const { channel } = msg.member.voice
+      const player = client.music.players.get(msg.guild.id)
+      if (!player) return msg.channel.send(new client.Embed().error('There is nothing playing!'))
+      if (channel.id !== player.voiceChannel.id || !channel) return msg.channel.send(new client.Embed().error('You are not in the same voice channel as me!'))
+  
+      player.pause(player.playing)
+      return msg.channel.send(new client.Embed().success(`The queue is now **${player.playing ? 'resumed' : 'paused'}**!`))
+    }
+  }
