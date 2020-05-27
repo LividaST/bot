6e7294b7cefbb059fe2b7a9720a0a085c9777418
Joshua Pemberton <joshp2865@gmail.com>
2020-05-27 21:20:16 +0100

    Linted and Testing Github

---
+++ src/Commands/Github/user.js
@@ -0,0 +1,29 @@
+module.exports = {
+  name: 'user',
+  aliases: [],
+  category: 'GitHub',
+  description: 'View information about a GitHub user',
+  usage: '<username>',
+  permissions: 'SEND_MESSAGES',
+  clientPerms: 'SEND_MESSAGES',
+  creatorOnly: false,
+  guildOnly: false,
+  premiumOnly: false,
+  requiresArgs: true,
+  run: (client, msg, args) => {
+    client.fetch(`https://api.github.com/users/${args}`).then(res => res.json())
+      .then(json => {
+        const embed = new client.Embed()
+          .setTitle(json.name)
+          .setURL(json.html_url)
+          .setThumbnail(json.avatar_url)
+          .addField('Followers', json.followers, true)
+          .addField('Following', json.following, true)
+          .addField('Public Repos', json.public_repos)
+          .setFooter('Created at')
+          .setTimestamp(json.created_at)
+          .setDescription(json.bio)
+        msg.channel.send(embed)
+      })
+  }
+}
