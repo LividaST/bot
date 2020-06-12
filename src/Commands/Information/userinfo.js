197cb766fdfd8563826e80dd0b974e1a16b57f1b
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2020-06-12 18:06:33 +0100

    Update userinfo.js

--- src/Commands/Information/userinfo.js
+++ src/Commands/Information/userinfo.js
@@ -8,38 +8,23 @@ module.exports = {
   permissions: 'SEND_MESSAGES',
   clientPerms: 'SEND_MESSAGES',
   creatorOnly: false,
-  guildOnly: false,
+  guildOnly: true,
   premiumOnly: false,
   requiresArgs: false,
   run: async (client, msg, args) => {
     try {
-      let user; let member
-      if (args[0]) {
-        if (msg.mentions.users.size) {
-          user = msg.mentions.users.first()
-          member = msg.mentions.members.first()
-        } else {
-          if (client.getUser(args[0])) {
-            user = client.getUser(args[0])
-            member = client.getMember(args[0], msg)
-          }
-        }
-      } else {
-        user = msg.author
-        member = msg.member
-      }
-
+      const user = client.getUser(args[0] || message.author),
+        member = client.getMember(args[0] || message.member, msg);
+      
       const embed = new client.Embed()
-        .setAuthor(`${user.username}'s Information`, member.user.avatarURL())
+        .setAuthor(`${user.username}'s Information`, user.avatarURL())
         .addField('**Status**', member.presence.status, true)
-        .addField('**Game**', member.user.presence.activities[0].name || 'None', true)
+        .addField('**Game**', user.presence.activities[0].name || 'None', true)
         .setFooter(`Requested by ${msg.author.tag}`)
       if (member.displayHexColor !== '#000000') embed.setColor(member.displayHexColor)
-      if (msg.channel.type !== 'dm') {
-        embed.addField(`**Roles [${msg.member.roles.cache.size}]**`, member.roles.cache.map(x => `${x.name}`).join(', '))
-        embed.addField('**Joined at**', moment(member.joinedTimestamp).format('dddd, Do MMMM, YYYY hh:mm a'), true)
-      }
-      embed.addField('**Registered at**', moment(user.createdTimestamp).format('dddd, Do MMMM, YYYY hh:mm a'), true)
+      embed.addField(`**Roles [${msg.member.roles.cache.size}]**`, member.roles.cache.map(x => `${x.name}`).join(', '))
+      embed.addField('**Joined at**', moment(member.joinedTimestamp).format('dddd, MMM DD, YYYY hh:mm a'), true)
+      embed.addField('**Registered at**', moment(user.createdTimestamp).format('dddd, MMM DD, YYYY hh:mm a'), true)
       msg.channel.send(embed)
     } catch {
       msg.channel.send(new client.Embed().error(`The specified user \`${args[0]}\` was not found!`))
