e9ee03742b8f994995b36c76563108db8bab54bb
Joshua Pemberton <joshp2865@gmail.com>
2020-06-12 15:17:16 +0100

    added some dodgy code

--- src/Commands/Information/profile.js
+++ src/Commands/Information/profile.js
@@ -1,3 +1,5 @@
+const { Canvas } = require('canvas-constructor')
+const { resolve, join } = require('path')
 module.exports = {
   name: 'profile',
   aliases: [],
@@ -14,11 +16,25 @@ module.exports = {
     var { UserProfile } = client.Models
     const data = await UserProfile.find({ userID: msg.author.id })
     const profile = data[0]
+
+    if (!profile) {
+      msg.channel.send(new client.Embed().error('No profile has been found, a profile will now be created for you.')).then(message => {
+        var query = { userID: msg.author.id }
+        UserProfile.findOneAndUpdate(query, { userID: msg.author.id }, { upsert: true }, function (err, doc) {
+          if (err) return msg.channel.send(err)
+          const embed = new client.Embed().setDescription('Profile Created')
+          message.edit(embed).then(message => {
+            message.delete({ timeout: 5000 })
+          })
+        })
+      })
+    }
     const user = client.users.cache.get(profile.userID)
+    const badges = (profile.verified ? client.Emojis.verified : '') + (profile.developer ? client.Emojis.developer : '') + (profile.staff ? client.Emojis.staff : '')
     const embed = new client.Embed()
       .setTitle(`${user.tag}'s Profile`)
-      .addField('Birthday', profile.birthday)
-    if (profile.position) embed.addField('Position', profile.position)
+      .setDescription(badges)
+    if (profile.birthday) embed.addField('Birthday', profile.birthday)
     msg.channel.send(embed)
   }
 }
