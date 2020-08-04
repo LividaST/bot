60410f23a030df4911f167fa651b5d937b6d3953
Joshua Pemberton <joshp2865@gmail.com>
2020-08-04 16:26:57 +0100

    Update pause.js
    will this fix it?

--- src/Commands/Music/pause.js
+++ src/Commands/Music/pause.js
@@ -13,8 +13,8 @@ module.exports = {
     const { channel } = msg.member.voice
     const player = client.music.players.get(msg.guild.id)
     if (msg.member.roles.cache.some(role => role.name === 'DJ') === 'DJ') return msg.channel.send(new client.Embed().error('You need to have the role called `DJ` to use this command.'))
-    if (!channel || channel.id !== player.voiceChannel.id) return msg.channel.send(new client.Embed().error('You need to be in the same voice channel as me to use the leave command!'))
     if (!player) return msg.channel.send(new client.Embed().error('No songs currently playing in this server!'))
+    if (!channel || channel.id !== player.voiceChannel.id) return msg.channel.send(new client.Embed().error('You need to be in the same voice channel as me to use the leave command!'))
 
     player.pause(player.playing)
     return msg.channel.send(new client.Embed().success(`The queue is now **${player.playing ? 'resumed' : 'paused'}**!`))
