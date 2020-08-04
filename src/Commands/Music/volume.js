52b21c20d4f475032cffe7c2cac292d56570540d
Joshua Pemberton <joshp2865@gmail.com>
2020-08-04 16:48:41 +0100

    stop
    stoopid

--- src/Commands/Music/volume.js
+++ src/Commands/Music/volume.js
@@ -12,7 +12,7 @@ module.exports = {
   run: async (client, msg, args) => {
     const player = client.music.players.get(msg.guild.id)
     const { channel } = msg.member.voice
-    if (msg.member.roles.cache.some(role => role.name === 'DJ') = false) return msg.channel.send(new client.Embed().error('You need to have the role called `DJ` to use this command.'))
+    if (msg.member.roles.cache.some(role => role.name === 'DJ') === false) return msg.channel.send(new client.Embed().error('You need to have the role called `DJ` to use this command.'))
     if (!player) return msg.channel.send(new client.Embed().error('No songs currently playing in this server!'))
     if (!channel || channel.id !== player.voiceChannel.id) return msg.channel.send(new client.Embed().error('You need to be in the same voice channel as me to use the leave command!'))
     if (!args[0]) return msg.channel.send(`Current volume: **${player.volume}**`)
