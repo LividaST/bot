b54418ff317aef62f1a5f14bf72597046772e406
Joshua Pemberton <joshp2865@gmail.com>
2020-08-06 20:39:54 +0100

    oops

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.post('/radioRequest', function (req, res) {
   const embed = new client.Embed()
     .setAuthor(request.entry.type)
     .setDescription(request.entry.content)
-    .addField('Name', request.name)
+    .addField('Name', request.entry.name)
     .setFooter(`ID: ${request.entry.id}`)
   client.channels.cache.get('740698608567058463').send(embed)
   res.json({ success: true })
