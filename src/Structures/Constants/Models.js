8682dd643d9634303a8b3fcb3e5ac72289fd3e20
Joshua Pemberton <joshp2865@gmail.com>
2020-09-12 16:10:36 +0100

    Remove profiles
    Signed-off-by: Joshua Pemberton <joshp2865@gmail.com>

--- src/Structures/Constants/Models.js
+++ src/Structures/Constants/Models.js
@@ -5,37 +5,6 @@ const prefixSchema = new mongoose.Schema({
   prefix: String
 })
 
-const UserProfile = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  userID: {
-    type: String
-  },
-  developer: {
-    type: Boolean,
-    default: false
-  },
-  staff: {
-    type: Boolean,
-    default: false
-  },
-  verified: {
-    type: Boolean,
-    default: false
-  },
-  birthday: {
-    type: Date
-  },
-  level: {
-    type: Number,
-    default: '0'
-  },
-  xp: {
-    type: Number,
-    default: '0'
-  }
-})
-
 module.exports = {
-  Prefix: mongoose.model('Prefix', prefixSchema),
-  UserProfile: mongoose.model('UserProfile', UserProfile)
+  Prefix: mongoose.model('Prefix', prefixSchema)
 }
