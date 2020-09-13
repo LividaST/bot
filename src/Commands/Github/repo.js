cd6e96a42f282177a07fb7746e2ef1d2cb7c8277
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2020-09-13 20:01:37 +0100

    Update repo.js

--- src/Commands/Github/repo.js
+++ src/Commands/Github/repo.js
@@ -11,6 +11,14 @@ module.exports = {
   premiumOnly: false,
   requiresArgs: true,
   run: (client, msg, args) => {
-    msg.channel.send('Someone remind me to finish this: MegaJoshy#0001')
+    if(!args[0]) return msg.reply("please specify a github profile!");
+    if(!args[1]) return msg.reply("please specify a repository name!");
+
+    client.fetch(`https://api.github.com/repos/${args[0]}/${args[1]}`).then(res => res.json())
+      .then(json => {
+        let embed = new client.Embed()
+          .setDescription(`Viewing information for github repository: [**${json.full_name}**](${json.html_url})\n**Description:** ${json.description}`)
+        message.channel.send(embed);
+    })
   }
 }
