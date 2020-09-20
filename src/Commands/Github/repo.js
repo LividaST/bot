d4ec38b43505e4b13beb6763589c7bed047b4ae5
Joshua Pemberton <joshp2865@gmail.com>
2020-09-21 00:02:59 +0100

    refactor(nowplaying): redone nowplaying commands

--- src/Commands/Github/repo.js
+++ src/Commands/Github/repo.js
@@ -11,15 +11,15 @@ module.exports = {
   premiumOnly: false,
   requiresArgs: true,
   run: (client, msg, args) => {
-    if(!args[0]) return msg.reply("please specify a github profile!");
-    if(!args[1]) return msg.reply("please specify a repository name!");
+    if (!args[0]) return msg.reply('please specify a github profile!')
+    if (!args[1]) return msg.reply('please specify a repository name!')
 
     client.fetch(`https://api.github.com/repos/${args[0]}/${args[1]}`).then(res => res.json())
       .then(json => {
-        if(json.message) return msg.reply("the specified repository was not found!");
-        let embed = new client.Embed()
+        if (json.message) return msg.reply('the specified repository was not found!')
+        const embed = new client.Embed()
           .setDescription(`Viewing information for github repository: [**${json.full_name}**](${json.html_url})\n**Description:** ${json.description}`)
-        msg.channel.send(embed);
-    })
+        msg.channel.send(embed)
+      })
   }
 }
