6ea6c45fdc9eecafc91a1fbd97724b623c5be491
Joshua Pemberton <joshp2865@gmail.com>
2020-09-21 14:05:03 +0100

    style: :art: Improve structure

--- src/Commands/Music/lyrics.js
+++ src/Commands/Music/lyrics.js
@@ -11,8 +11,8 @@ module.exports = {
   premiumOnly: false,
   requiresArgs: false,
   run: async (client, msg, args) => {
-    const { data } = await client.fetch('https://api.livida.net/api/radio').then(res => res.json())
-    const lyrics = await client.ksoft.lyrics.get(data.song.name + data.song.artist).then(x => x)
-    msg.channel.send(`**${data.song.name} - ${data.song.artist}**\n${lyrics.lyrics}`, { split: true })
+    const { data: { song } } = await client.fetch('https://api.livida.net/api/radio').then(res => res.json())
+    const lyrics = await client.ksoft.lyrics.get(song.name + song.artist).then(x => x)
+    msg.channel.send(`**${song.name} - ${song.artist}**\n${lyrics.lyrics}`, { split: true })
   }
 }
