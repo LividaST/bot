c97acf52f46b408837acfcb033852a92338d1de7
Joshua Pemberton <joshp2865@gmail.com>
2020-09-21 14:01:15 +0100

    feat: :sparkles: Added lyrics command for nowplaying command

--- src/Commands/Music/lyrics.js
+++ src/Commands/Music/lyrics.js
@@ -3,14 +3,16 @@ module.exports = {
   aliases: [],
   category: 'Music',
   description: 'Get lyrics of current song!',
-  usage: '<song title>',
+  usage: '',
   permissions: 'SEND_MESSAGES',
   clientPerms: 'SEND_MESSAGES',
-  creatorOnly: true,
+  creatorOnly: false,
   guildOnly: false,
   premiumOnly: false,
   requiresArgs: false,
   run: async (client, msg, args) => {
-    msg.channel.send('Will be sorted out soon.')
+    const { data } = await client.fetch('https://api.livida.net/api/radio').then(res => res.json())
+    const lyrics = await client.ksoft.lyrics.get(data.song.name + data.song.artist).then(x => x)
+    msg.channel.send(`**${data.song.name} - ${data.song.artist}**\n${lyrics.lyrics}`, { split: true })
   }
 }
