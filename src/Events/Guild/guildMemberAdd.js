65cae4d7f2de70385ba518c9889ec13bb1f6c27d
Joshua Pemberton <joshp2865@gmail.com>
2020-10-01 19:22:50 +0100

    feat(bot): welcome messages for eSports

--- src/Events/Guild/guildMemberAdd.js
+++ src/Events/Guild/guildMemberAdd.js
@@ -10,5 +10,13 @@ module.exports = {
         .addField('Membercount', guild.memberCount)
       guild.channels.cache.get('696081501044867143').send(member, embed)
     }
+    if (guild.id === '707743353810190386') {
+      const embed = new client.Embed()
+        .setTitle('Welcome to Livida | eSports and Gaming!')
+        .setURL('https://livida.net')
+        .setDescription(`${member.user.username}, welcome to Livida! If you need any help feel free to message <@544943898980646922>.`)
+        .addField('Membercount', guild.memberCount)
+      guild.channels.cache.get('761291544786370570').send(member, embed)
+    }
   }
 }
