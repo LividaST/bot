7c8d10c69c32450ebb7d3a3d1345ac12b1996291
callumok2004 <callumokane123@gmail.com>
2021-01-04 21:30:42 +0000

    ok

--- dist/Util/Functions/Fetch.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
-exports.default = (url, options) => axios_1.default(url, options);
-//# sourceMappingURL=Fetch.js.map
--- dist/config.js
+++
@@ -1,2 +0,0 @@
-module.exports = {};
-//# sourceMappingURL=config.js.map
