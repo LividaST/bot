ba85bca354c5d5d7b5ac484f2af3545b91874978
callumok2004 <callumokane123@gmail.com>
2021-02-05 20:02:27 +0000

    lala

--- src/Commands/Radio/volume.ts
+++ src/Commands/Radio/volume.ts
@@ -11,12 +11,12 @@ module.exports = {
     },
     run: async (client, message, args) => {
         const voiceChannel = message.member.voice.channel, dispatcher = client.music.get(message.guild.id);
-        if (!voiceChannel) return message.channel.send(new client.Embed(message.guild.id).setDescription("You need to be in a voice channel to use this command!"));
-        if(!args[0]) return message.channel.send(new client.Embed(message.guild.id).setDescription(`The volume is currently set to **${dispatcher.volume}**!`));
-        if (args[0] < 0) return message.channel.send(new client.Embed(message.guild.id).setDescription(`Cannot set volume below 0, the volume is currently **${dispatcher.volume}**!`));
-        if (args[0] > 5) return message.channel.send(new client.Embed(message.guild.id).setDescription(`Cannot set volume above 5, the volume is currently **${dispatcher.volume}**!`));
+        if (!voiceChannel) return message.channel.send(new client.Embed().setDescription("You need to be in a voice channel to use this command!"));
+        if(!args[0]) return message.channel.send(new client.Embed().setDescription(`Please specify a new volume!`));
+        if (args[0] < 0) return message.channel.send(new client.Embed().setDescription(`Cannot set volume below 0!`));
+        if (args[0] > 5) return message.channel.send(new client.Embed().setDescription(`Cannot set volume above 5!`));
         dispatcher.setVolumeLogarithmic(args[0] / 5);
-        message.channel.send(new client.Embed(message.guild.id).setDescription(`Volume has been updated to **${args[0]}**`));
+        message.channel.send(new client.Embed().setDescription(`Volume has been updated to **${args[0]}**`));
 
     }
 }
